5c8f7e8abc76fb22c4d81122dec323d340ce039d
xinba123 <xinba1989918@gmail.com>
2016-07-31 22:42:12 +0800

    add angular for search

---
+++ js/searchController.js
@@ -0,0 +1,16 @@
+var app = angular.module('MonaGO', []);
+
+app.constant('goNodes', goNodes);
+
+app.controller('searchCtrl', ['$scope', 'goNodes', function($scope,goNodes) {
+    $scope.goNodes = goNodes
+
+    $scope.isNotEmpty = function(searchText){
+        if (searchText.length!=0){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
