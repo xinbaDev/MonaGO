8da8c8225055fe7389ab97c195955a2ef988043b
xinba123 <xinba1989918@gmail.com>
2016-07-31 23:02:14 +0800

    fix arrow bugs

--- js/searchController.js
+++ js/searchController.js
@@ -3,9 +3,10 @@ var app = angular.module('MonaGO', []);
 app.constant('goNodes', goNodes);
 
 app.controller('searchCtrl', ['$scope', 'goNodes', function($scope,goNodes) {
-    $scope.goNodes = goNodes
+    $scope.goNodes = goNodes;
 
     $scope.isNotEmpty = function(searchText){
+
         if (searchText.length!=0){
             return true;
         }
@@ -13,4 +14,4 @@ app.controller('searchCtrl', ['$scope', 'goNodes', function($scope,goNodes) {
             return false;
         }
     }
-}
+}]);
